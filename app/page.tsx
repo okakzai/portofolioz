b8e041c04d83e77e40d1448caadfcb048f304e62
unknown <okakzai@gmail.com>
2025-02-13 11:01:17 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-black-100 text-white text-center">
+      <h1>
+        Website Portfolio Modern Menggunakan Node.js, Next.js, TypeScript,
+        Three.js, Tailwind CSS, Aceternity UI (ShadCN + Framer Motion) dan
+        Sentry.io
+      </h1>
+    </main>
+  );
+}
